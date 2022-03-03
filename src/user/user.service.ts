3858acfd8a5315b42dc1c3f2550bcc9896804747
Faizan Ahmed <syedfaizan.dev@gmail.com>
2022-03-03 10:35:26 +0500

    init

---
+++ src/user/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from './dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) { }
+
+  getUser(userId: number) {
+    return this.prisma.user.findMany({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        createdAt: true,
+        updatedAt: true,
+        bookmarks: {
+          select: {
+            id: true,
+            title: true,
+            link: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      }
+    });
+  }
+
+  async editUser(
+    userId: number,
+    dto: EditUserDto,
+  ) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+}
